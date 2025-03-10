cac7a3c6e369577f3967ef5e81ff0f4b739464f3
John Vo <vochidung243@gmail.com>
2025-03-10 22:30:50 +0700

    John updated

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import helper
 import pandas as pd
 
 df = pd.read_csv("poems_dataset_proc0.csv")
-parts = helper.split_df(df, 2)
+parts = helper.split_df(df, 10)
 for i in range(len(parts)):
     parts[i].to_csv(f"poems_dataset_proc0_{i}.csv", index=False, encoding="utf-8")
 

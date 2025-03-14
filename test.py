0c3bb74b37c3a5b28c470a9f493c8731ce34d565
John Vo <vochidung243@gmail.com>
2025-03-14 21:19:13 +0700

    updated

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import pandas as pd
 import os
 
 ## SPLIT DATASET TO N PARTS
-# df = pd.read_csv("poems_dataset_proc0.csv")
-# parts = helper.split_df(df, 10)
+# df = pd.read_csv("poems_dataset_proc0_5.csv")
+# parts = helper.split_df(df, 6)
 # for i in range(len(parts)):
 #     parts[i].to_csv(f"poems_dataset_proc0_{i}.csv", index=False, encoding="utf-8")
 
@@ -17,7 +17,9 @@ import os
 # pd.DataFrame(authors_not_in_thivien, columns=["Author"]).to_csv("authors_in_thivien.csv", index=False)
 # print(df.count("index")["Genre"])
 
+df0 = pd.read_csv(f"handled_dataset/poems_dataset_processed.csv")
 dfs = []
+print(len(df0))
 for i in range(0, 5):
   try:
     df_i = pd.read_csv(f"poems_dataset_proc0_{i}_handled.csv")
@@ -26,7 +28,7 @@ for i in range(0, 5):
   except Exception:
     print("Invalid")
     continue
-df = helper.merge_dataframes(dfs)
+df = helper.merge_dataframes([df0,] + dfs)
 df = df[df["Genre"].notna() & (df["Genre"] != "Xóa")].drop_duplicates(["Edited"])
 print(len(df))
 df.to_csv("handled_dataset/poems_dataset_processed.csv", index=False)

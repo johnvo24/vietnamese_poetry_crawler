a0356a7c4769c6518a6e8e4d59d7bf5c4c8e757b
chjcfcaloc2020 <nguyentruongchi83@gmail.com>
2025-03-16 00:38:52 +0700

    chi commited

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def merge_dataset(from_, to_, file_="poems_dataset_proc0"):
 ## Chia file poems_dataset_proc1.csv thành: từ "poems_dataset_proc1_0.csv" đến "poems_dataset_proc1_5.csv"
 ## Thì dùng: split_dataset(file_="poems_dataset_proc1", n_parts_=6)
 
-handle_dataset(from_=0, to_=20, file_="poems_dataset_proc1", driver_type_="firefox", num_processes_=1, allow_overwrite=False)
+handle_dataset(from_=0, to_=20, file_="poems_dataset_proc2", driver_type_="chrome", num_processes_=1, allow_overwrite=False)
 ## >>> Example:
 ## Xử lý file: từ "poems_dataset_proc1_0.csv" đến "poems_dataset_proc1_5.csv" thành "poems_dataset_proc1_0_handled.csv" ... "poems_dataset_proc1_5_handled.csv"
 ## Thì dùng: handle_dataset(from_=0, to_=5, file_="poems_dataset_proc1", driver_type_="firefox", num_processes_=1, allow_overwrite=False)

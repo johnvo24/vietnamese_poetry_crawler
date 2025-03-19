dcd54ec1785d183052e9d2e68b0bc6f896a6c5a4
John Vo <vochidung243@gmail.com>
2025-03-19 23:17:46 +0700

    John updated

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-# TEST FUNCTION
-import pandas as pd
-import random
-import os
+from underthesea import word_tokenize, ner
 
+# Ví dụ bài thơ có địa danh
+poem = "Đừng lặng im"
 
-# df0 = pd.read_csv("poems_dataset_proc0_0_handled.csv")
-# df1 = pd.read_csv("poems_dataset_proc0_1_handled.csv")
-# df2 = pd.read_csv("poems_dataset_proc0_2_handled.csv")
-# df = pd.concat([df0, df1, df2])
-# authors_not_in_thivien = df.loc[df["Genre"].notna(), "Author"].dropna().str.lower().unique()
-# pd.DataFrame(authors_not_in_thivien, columns=["Author"]).to_csv("authors_in_thivien.csv", index=False)
-# print(df.count("index")["Genre"])
+# Tokenize và nhận diện thực thể
+tokens = word_tokenize(poem)
+entities = ner(poem)
 
-# df = pd.read_csv("poems_dataset_proc1_0.csv")
-# print(len(df.drop_duplicates(["Edited"])))
+print("Tokens:", tokens)
+print("Entities:", entities)

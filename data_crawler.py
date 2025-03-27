e8bbeaa0969f365efa2a1811db91f577660eba01
John Vo <vochidung243@gmail.com>
2025-03-27 17:33:10 +0700

    John updated

--- data_crawler.py
+++ data_crawler.py
@@ -46,7 +46,7 @@ class DataCrawler:
     else: target = genre
     return target
   
-  def crawl_data(self, start_author_, amount_):
+  def crawl_data(self, start_author_, amount_, df_target_):
     data = []
     driver = self.get_driver()
     request_count = 0
@@ -65,11 +65,42 @@ class DataCrawler:
         request_count = 0
         time.sleep(random.uniform(60, 120))
       
-      for i in range(0, 10):
-        # SEARCH AUTHOR WITH POEM
-        author_url = f"https://www.thivien.net/searchpoem.php??Title={str(author_row['Title']).lower()}&Author={author}&ViewType=1&Country=2"
+      # SEARCH AUTHOR WITH POEM
+      author_url = f"https://www.thivien.net/searchpoem.php??Title={str(author_row['Title']).lower()}&Author={author}&ViewType=1&Country=2"
+      driver.get(author_url)
+      request_count += 1
+      helper.delay()
+
+      # Kiểm tra nếu bị chặn bởi CAPTCHA
+      while "xác nhận không phải máy" in driver.page_source.lower() or "tần suất quá cao" in driver.page_source.lower():
+        if "tần suất quá cao" in driver.page_source.lower():
+          print("🔒 Bị chặn truy cập!!!")
+          time.sleep(random.uniform(180, 240))
+          ## Change proxy
+          driver.quit()
+          driver = self.get_driver()
+        else:
+          print("🔒 Phát hiện CAPTCHA!!!")
+          time.sleep(random.uniform(60, 65))
         driver.get(author_url)
         request_count += 1
+        helper.delay()
+
+      author_soup = BeautifulSoup(driver.page_source, "html.parser")
+      poems_of_author = author_soup.find_all("div", class_="list-item-detail")
+      if len(poems_of_author) < 1:
+        print(f"E1: Author not found: {author}")
+        continue
+
+      print(f"#####: Author: {author}")
+      for poem in poems_of_author:
+        a_tag = poem.find_all("a")[2]
+        if a_tag.get_text().lower() != author: continue
+        poem_author = a_tag.get_text()
+
+        # SEARCH AUTHOR WITH AUTHOR URL
+        driver.get(f"{self.base_url}/{a_tag['href']}")
+        request_count +=1
         helper.delay()
 
         # Kiểm tra nếu bị chặn bởi CAPTCHA
@@ -83,83 +114,63 @@ class DataCrawler:
           else:
             print("🔒 Phát hiện CAPTCHA!!!")
             time.sleep(random.uniform(60, 65))
-          driver.get(author_url)
+          driver.get(f"{self.base_url}/{a_tag['href']}")
           request_count += 1
           helper.delay()
+        
+        # GET POEM
+        author_page_soup = BeautifulSoup(driver.page_source, "html.parser")
+        poem_group_lists = author_page_soup.find_all("div", class_="poem-group-list")
 
-        author_soup = BeautifulSoup(driver.page_source, "html.parser")
-        poems_of_author = author_soup.find_all("div", class_="list-item-detail")
-        if len(poems_of_author) < 1:
-          print(f"E1: Author not found: {author}")
-          continue
+        translated_poem_group_title = author_page_soup.find("h4", class_="poem-group-title", string="Thơ dịch tác giả khác")
+        if translated_poem_group_title: source_line_limit = translated_poem_group_title.sourceline;
+        else: source_line_limit = 99999
+        print(f"Line limit: {source_line_limit}")
 
-        print(f"#####: Author: {author}")
-        for poem in poems_of_author:
-          a_tag = poem.find_all("a")[2]
-          if a_tag.get_text().lower() != author: continue
+        for poem_group_list in poem_group_lists:
+          if poem_group_list.sourceline > source_line_limit: 
+            print(f"WN: The line limit has been reached at {source_line_limit}.")
+            break
 
-          # SEARCH AUTHOR WITH AUTHOR URL
-          driver.get(f"{self.base_url}/{a_tag['href']}")
-          request_count +=1
-          helper.delay()
+          poem_tag_list = poem_group_list.find_all('a')
+          for poem_tag in poem_tag_list:
+            poem_title = poem_tag.get_text()
+            if (poem_title.lower() in self.poems_dataset_processed["Title"].to_list()) or (poem_title in df_target_["Title"].to_list()): 
+              print(f"E: {author} - {poem_title} is already!")
+              continue
 
-          # Kiểm tra nếu bị chặn bởi CAPTCHA
-          while "xác nhận không phải máy" in driver.page_source.lower() or "tần suất quá cao" in driver.page_source.lower():
-            if "tần suất quá cao" in driver.page_source.lower():
-              print("🔒 Bị chặn truy cập!!!")
-              time.sleep(random.uniform(180, 240))
-              ## Change proxy
-              driver.quit()
-              driver = self.get_driver()
-            else:
-              print("🔒 Phát hiện CAPTCHA!!!")
-              time.sleep(random.uniform(60, 65))
-            driver.get(f"{self.base_url}/{a_tag['href']}")
-            request_count += 1
-            helper.delay()
-          
-          # GET POEM
-          author_page_soup = BeautifulSoup(driver.page_source, "html.parser")
-          poem_group_lists = author_page_soup.find_all("div", class_="poem-group-list")
-          for poem_group_list in poem_group_lists:
-            poem_tag_list = poem_group_list.find_all('a')
-            for poem_tag in poem_tag_list:
-              poem_title = poem_tag.get_text()
-              if poem_title in self.poems_dataset_processed["Title"].to_list(): 
-                print(f"E: {author} - {poem_title} is already!")
-                continue
+            poem_url = f"{self.base_url}/{poem_tag['href']}"
+            driver.get(poem_url)
+            request_count +=1
+            helper.delay(2, 5)
 
-              poem_url = f"{self.base_url}/{poem_tag['href']}"
+            # Kiểm tra nếu bị chặn bởi CAPTCHA
+            while "xác nhận không phải máy" in driver.page_source.lower() or "tần suất quá cao" in driver.page_source.lower():
+              if "tần suất quá cao" in driver.page_source.lower():
+                print("🔒 Bị chặn truy cập!!!")
+                time.sleep(random.uniform(180, 240))
+                ## Change proxy
+                driver.quit()
+                driver = self.get_driver()
+              else:
+                print("🔒 Phát hiện CAPTCHA!!!")
+                time.sleep(random.uniform(60, 65))
               driver.get(poem_url)
-              request_count +=1
-              helper.delay(2, 5)
+              request_count += 1
+              helper.delay()
 
-              # Kiểm tra nếu bị chặn bởi CAPTCHA
-              while "xác nhận không phải máy" in driver.page_source.lower() or "tần suất quá cao" in driver.page_source.lower():
-                if "tần suất quá cao" in driver.page_source.lower():
-                  print("🔒 Bị chặn truy cập!!!")
-                  time.sleep(random.uniform(180, 240))
-                  ## Change proxy
-                  driver.quit()
-                  driver = self.get_driver()
-                else:
-                  print("🔒 Phát hiện CAPTCHA!!!")
-                  time.sleep(random.uniform(60, 65))
-                driver.get(poem_url)
-                request_count += 1
-                helper.delay()
-
-              poem_soup = BeautifulSoup(driver.page_source, "html.parser")
-              summary_section = poem_soup.find("div", class_="summary-section")
-              if summary_section:
-                poem_genre = summary_section.find("a").get_text()
-                content_tag = poem_soup.find("div", class_="poem-content")
-
+            poem_soup = BeautifulSoup(driver.page_source, "html.parser")
+            summary_section = poem_soup.find("div", class_="summary-section")
+            if summary_section and summary_section.find("a"):
+              poem_genre = summary_section.find("a").get_text()
+              content_tag = poem_soup.find("div", class_="poem-content")
+              if content_tag:
                 data.append({
                     'Original': "",
                     'Edited': BeautifulSoup(content_tag.find('p').decode_contents().replace("<br/>", "\n"), "html.parser").get_text(),
                     'Title': poem_title,
                     'Genre': self.convert_poem_genre(poem_genre),
+                    'Author': poem_author,
                     'URL': poem_url
                 })
                 poem_count += 1
@@ -168,34 +179,35 @@ class DataCrawler:
                   driver.quit()
                   print(f"Data crawled successfully!")
                   return pd.DataFrame(data=data)
-          break
+        break
 
     driver.quit()
     print(f"Data crawled successfully!")
     return pd.DataFrame(data=data)
 
   def start(self, start_author, num_loop=10, amount=250):
-      print(f"==> Crawling dataset... ")
-      start_author_ = start_author
-      data_0 = []
-      for i in range(0, num_loop):
-        print(f"############### LOOP {i+1} ###############")
-        try:
-          data_0 = pd.read_csv(f"handled_dataset/poems_dataset_processed_0.csv")
-        except ValueError:
-          pass
-        print(f"Dataset: {len(data_0)}")
-        if data_0:
-          start_author_ = str(data_0.loc[-1]['Author']).lower()
-        else:
-          data_0 = pd.DataFrame(data=[])
-        
-        result = self.crawl_data(start_author_=start_author_, amount_=amount)
+    print(f"==> Crawling dataset... ")
+    start_author_ = start_author
+    data_0 = []
+    for i in range(0, num_loop):
+      print(f"############### LOOP {i+1} ###############")
+      try:
+        data_0 = pd.read_csv(f"handled_dataset/poems_dataset_processed_0.csv")
+      except ValueError:
+        pass
+      print(f"Dataset: {len(data_0)}")
 
-        df = helper.merge_dataframes([data_0, result])
-        df.to_csv(f"handled_dataset/poems_dataset_processed_0.csv", index=False, encoding="utf-8")
-        print("ZZZ... Waiting for saving dataset")
-        helper.delay(30, 35)
+      if len(data_0) > 0:
+        start_author_ = str(data_0.iloc[-1]['Author']).lower()
+      else:
+        data_0 = pd.DataFrame(data=[])
+      
+      result = self.crawl_data(start_author_=start_author_, amount_=amount, df_target_=data_0)
+
+      df = helper.merge_dataframes([data_0, result])
+      df.to_csv(f"handled_dataset/poems_dataset_processed_0.csv", index=False, encoding="utf-8")
+      print("ZZZ... Waiting for saving dataset")
+      helper.delay(30, 35)
       
 crawler = DataCrawler(driver_type="firefox", num_processes=1)
 crawler.start(start_author='hư vô', num_loop=10, amount=500)
